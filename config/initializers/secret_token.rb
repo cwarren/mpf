b809c2110c5917669139c619f7a3fb631b880164
Chris Warren <csw11235@gmail.com>
2011-05-25 15:51:23 -0400

    initial rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mpf::Application.config.secret_token = 'afec1154c865eb8b5f4ae0e5533b46c14de7bddfdbb7bf130e0fe18973e26f1f343229328e876b85e688f91e4f286085f0509ae48152e74a75fa6aa948ec72d1'
