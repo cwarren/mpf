325ae19e2d09c8f3bfb09ea62ef679101e6c453c
Chris Warren <csw11235@gmail.com>
2011-05-31 15:11:52 -0400

    basic admin namespace tests for pages working

--- spec/controllers/admin/pages_controller_spec.rb
+++ spec/controllers/admin/pages_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
-describe PagesController do
+describe Admin::PagesController do
+
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    it "should have the correct title" do
+      get 'home'
+      response.should have_selector("title", :content => "Williams College MPF : Admin Home")
     end
   end
 
@@ -14,12 +20,20 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+    it "should have the correct title" do
+      get 'home'
+      response.should have_selector("title", :content => "Williams College MPF : Admin About")
+    end
   end
 
   describe "GET 'help'" do
     it "should be successful" do
       get 'help'
       response.should be_success
+    end
+    it "should have the correct title" do
+      get 'home'
+      response.should have_selector("title", :content => "Williams College MPF : Admin Help")
     end
   end
 
