d1e32954bdd387e6514acc65c2dc88c7e4572266
Chris Warren <csw11235@gmail.com>
2011-06-01 12:09:05 -0400

    added a uniqueness test for mobile service model

--- spec/models/mobile_service_spec.rb
+++ spec/models/mobile_service_spec.rb
@@ -2,6 +2,16 @@ require 'spec_helper'
 
 describe MobileService do
   
+  before do
+    @ms_complete_in_db = MobileService.create!({
+                                                :title => "complete_ms_db",
+                                                :description => "a mobile service with all fields valid, though not necessarily meaningful",
+                                                :icon=>"msicons/foo.png",
+                                                :url=>"http://foo/ms",
+                                                :is_live=>true,
+                                                :is_restricted=>false})
+  end
+  
   before :each do
     @ms_complete = MobileService.new(title:"complete_ms",
                                      description:"a mobile service with all fields valid, though not necessarily meaningful",
@@ -23,4 +33,10 @@ describe MobileService do
     @ms_complete.should_not be_valid
   end
 
+  it "should be invalid with a non-unique title" do
+    @ms_complete.should be_valid
+    @ms_complete.title = "complete_ms_db"
+    @ms_complete.should_not be_valid
+  end
+  
 end
