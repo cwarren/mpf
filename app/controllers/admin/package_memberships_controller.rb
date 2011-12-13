89ae0e66c123c1fadee8212805aafa6d63b8ae48
Chris Warren <cwarren@williams.edu>
2011-12-13 15:57:33 -0500

    package membership management working

--- app/controllers/admin/package_memberships_controller.rb
+++ app/controllers/admin/package_memberships_controller.rb
@@ -1,11 +1,48 @@
-class Admin::PackageMembershipsController < ApplicationController
-  def create
+class Admin::PackageMembershipsController < Admin::AdminController
+
+  def create    
+    package_membership = PackageMembership.new do |pm|
+      pm.service_package_id = params[:package]
+      pm.package_item_id = params[:item]
+      pm.package_item_type = params[:item_type] 
+    end
+    
+    pmax = (PackageMembership.find_by_service_package_id(params[:package], {:order => 'ordering DESC'}))
+    max_existing_ordering = pmax.nil? ? 0 : pmax.ordering
+    package_membership.ordering = max_existing_ordering + 1
+    
+    if (package_membership.save)
+      respond_to do |format|
+        format.js { render :json => package_membership.id.to_json }
+      end
+    else
+      return false
+    end
+  end
+  
+
+  # NOTE: this ONLY updates the ordering!
+  def update
+    package_membership = PackageMembership.find(params[:id])
+    package_membership.ordering = params[:new_idx]
+    if (package_membership.save)
+      respond_to do |format|
+        format.js { render :json => package_membership.id.to_json }
+      end
+    else
+      return false
+    end
   end
 
-  def update
-  end
 
   def destroy
+    pm = PackageMembership.find(params[:id])
+    pm.destroy
+    
+    respond_to do |format|
+      format.js { render :nothing => true }
+    end
+
   end
 
 end
