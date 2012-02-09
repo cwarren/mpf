029314e39d91e0287fece5603b0d55fb29c8497a
Chris Warren <cwarren@williams.edu>
2012-02-09 10:32:38 -0500

    removing multistage deployment stuff from here

--- config/deploy/production.rb
+++
@@ -1,4 +0,0 @@
-set :rails_env, "production" 
-role :web, "mobile-web1.williams.edu"
-role :app, "mobile-web1.williams.edu"
-role :db,  "mobile-web1.williams.edu", :primary => true
--- config/deploy/staging.rb
+++
@@ -1,4 +0,0 @@
-set :rails_env, "production" 
-role :web, "mobile-webdev.williams.edu"
-role :app, "mobile-webdev.williams.edu"
-role :db,  "mobile-webdev.williams.edu", :primary => true
